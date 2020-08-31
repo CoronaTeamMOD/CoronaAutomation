c8d46d7e1a6fdabcd49b370e230263ea1b7ad649
yoavhizkyia <yoavhizkyia@gmail.com>
2020-08-31 12:44:18 +0300

    Adding Selectors and change the cypress resulation

--- cypress/integration/PagesSelectors/BasePageSelectors.js
+++ cypress/integration/PagesSelectors/BasePageSelectors.js
@@ -4,5 +4,10 @@ export default {
     PERSONAL_INFORMATION_TAB : ":nth-child(1) > .MuiTab-wrapper",
     INSULATION_AND_CLINICAL_DETAILS_TAB : ':nth-child(2) > .MuiTab-wrapper',
     POSSIBLE_EXPOSURE_TAB : ':nth-child(3) > .MuiTab-wrapper',
-    PLASCES_AND_CONTACTS : ':nth-child(4) > .MuiTab-wrapper'    
+    PLASCES_AND_CONTACTS : ':nth-child(4) > .MuiTab-wrapper',
+    QUIT_INVESTIGATION : ".makeStyles-managementControllers-18 > .MuiButton-root > .MuiButton-label",
+    YES_CONTINUE_POPUP : ".swal2-popup > .swal2-actions > swal2-confirm",
+    CANCEL_POPUP : ".swal2-popup > .swal2-actions > swal2-cancel",
+    NO_ANSWER_ON_THIS_PHONE_CHECKBOX : '.MuiFormControlLabel-root > .MuiTypography-root',
+    CONTINUE_TO_NEXT_TAB_BUTTON : ".makeStyles-buttonSection-8 > .MuiButtonBase-root > .MuiButton-label",
 }
